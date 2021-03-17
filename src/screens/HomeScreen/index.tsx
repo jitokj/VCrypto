ac0447181c83f0f4f88c9ef8f5011363af1ba8db
jito <jitokj3@gmail.com>
2021-03-17 19:18:07 +0530

    added homeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native';
+const image = require("../../../assets/images/images/Saly-1.png");
 
 const HomeScreen = () => {
     return (
-        <View style={styles.screen}>
-            <Text>HomeScreen</Text>
+        <View style={styles.root}>
+            <Image style={styles.image} source={image} />
+            <Text style={styles.title}>Welcome to VCrypto</Text>
+            <Text style={styles.subtitle}>Invest your virtual $100.00 and compete with others</Text>
         </View>
     )
 }
@@ -12,9 +15,25 @@ const HomeScreen = () => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+    root: {
+      padding: 20,
+      alignItems: "center",
+      flex: 1,
+      backgroundColor: "white"
+    },
+    image:{
+        height: "40%",
+        aspectRatio: 1
+    },
+    title:{
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 50,
+        marginBottom: 15
+    },
+    subtitle:{
+        fontSize: 20,
+        textAlign: "center",
+        color: "#707070"
     }
 })
